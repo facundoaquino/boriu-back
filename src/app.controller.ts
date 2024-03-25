b0905e4bb367485b0d88897dac8de6b01c590403
facundo aquino <facun90@live.com>
2024-03-25 17:45:27 -0300

    feat: Add endpoint to save order

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller()
 export class AppController {
@@ -7,6 +16,40 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getProducts();
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      limits: {
+        fileSize: 50 * 1024 * 1024, // Límite de tamaño del archivo en bytes (50 MB)
+      },
+      fileFilter: (req, file, callback) => {
+        if (
+          !file.originalname.match(
+            /\.(jpg|jpeg|png|gif|svg|bmp|webp|tiff|ico|psd|ai|eps)$/,
+          )
+        ) {
+          return callback(
+            new BadRequestException('Solo se permiten archivos de imagen.'),
+            false,
+          );
+        }
+        callback(null, true);
+      },
+    }),
+  )
+  uploadFile(
+    @UploadedFile() logo: Express.Multer.File,
+    @Body('name') name: string,
+    @Body('lastname') lastname: string,
+  ) {
+    try {
+      const result = this.appService.saveOrder(name, lastname, logo);
+      return { status: 'success', message: result };
+    } catch (error) {
+      return { status: 'error', message: 'Error al guardar la orden.' };
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  getProducts(): string {
     const data = fs.readFileSync('./files/products.json', 'utf8');
 
     return JSON.parse(data);
+  }
+
+  saveOrder(name: string, lastname: string, file: Express.Multer.File): string {
+    const directory = `./orders/${name}_${lastname}_${Date.now()}`; // Directorio para la orden
+    const fileName = `${name}_${lastname}.${file.originalname.split('.').pop()}`; // Nombre del archivo
+
+    try {
+      // Crea el directorio
+      fs.mkdirSync(directory, { recursive: true });
+
+      // Guarda el archivo en el directorio
+      fs.writeFileSync(`${directory}/${fileName}`, file.buffer);
+
+      return 'Orden guardada exitosamente.';
+    } catch (error) {
+      // Manejo de errores
+      console.error('Error al guardar la orden:', error);
+      return 'Error al guardar la orden.';
+    }
   }
 }
