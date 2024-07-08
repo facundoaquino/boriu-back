d41fd4b9987a7bde1ffca567e59723b40743508a
facundo aquino <facun90@live.com>
2024-07-08 18:03:22 -0300

    feat: adding cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://br4848.facuaquino.com.ar/', // Reemplaza con la URL permitida
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
